f9d04628e5aab2ac9549b32175b419a2c27a4b02
David Négrier <d.negrier@thecodingmachine.com>
2015-01-21 10:27:25 +0100

    Adding support for dependencies update in Mouf UI via EmbeddedComposer

--- src/MoufPlugin.php
+++ src/MoufPlugin.php
@@ -1,58 +1,211 @@
 <?php
 namespace Mouf\Installer;
-
 use Composer\Composer;
 use Composer\IO\IOInterface;
 use Composer\Script\Event;
 use Composer\Package\PackageInterface;
-use Composer\Package\AliasPackage;
 use Composer\Util\Filesystem;
 use Composer\Package\Dumper\ArrayDumper;
 use Composer\Plugin\PluginInterface;
+use Composer\Repository\CompositeRepository;
+use Composer\Package\CompletePackage;
+use Composer\Package\RootPackage;
+use Composer\Json\JsonFile;
+use Composer\Script\ScriptEvents;
+use Composer\EventDispatcher\EventSubscriberInterface;
 
 /**
  * RootContainer Installer for Composer.
  * (based on RobLoach's code for ComponentInstaller)
  */
-class MoufPlugin implements PluginInterface
-{
+class MoufPlugin implements PluginInterface, EventSubscriberInterface {
 
-    public function activate(Composer $composer, IOInterface $io)
-    {
-    	$moufFrameworkInstaller = new MoufFrameworkInstaller($io, $composer);
-    	$composer->getInstallationManager()->addInstaller($moufFrameworkInstaller);
-    	
-    	$moufLibraryInstaller = new MoufLibraryInstaller($io, $composer);
-    	$composer->getInstallationManager()->addInstaller($moufLibraryInstaller);
-    	
-    	// Now, let's register a script that saves the PHP binary path.
-        $rootPackage = $composer->getPackage();
-        if (isset($rootPackage)) {
-            // Ensure we get the root package rather than its alias.
-            while ($rootPackage instanceof AliasPackage) {
-                $rootPackage = $rootPackage->getAliasOf();
-            }
+	public function activate(Composer $composer, IOInterface $io) {
+		$moufFrameworkInstaller = new MoufFrameworkInstaller($io, $composer);
+		$composer->getInstallationManager()
+				->addInstaller($moufFrameworkInstaller);
 
-            // Make sure the root package can override the available scripts.
-            if (method_exists($rootPackage, 'setScripts')) {
-                $scripts = $rootPackage->getScripts();
-                // Act on the "post-autoload-dump" command so that we can act on all
-                // the installed packages.
-                $scripts['post-autoload-dump']['rootcontainer-installer'] = 'Mouf\\Installer\\MoufPlugin::postAutoloadDump';
-                $rootPackage->setScripts($scripts);
-            }
-        }
-    }
+		$moufLibraryInstaller = new MoufLibraryInstaller($io, $composer);
+		$composer->getInstallationManager()
+				->addInstaller($moufLibraryInstaller);
 
-    /**
-     * Script callback; Acted on after the autoloader is dumped.
-     */
-    public static function postAutoloadDump(Event $event)
-    {
-        // Retrieve basic information about the environment and present a
-        // message to the user.
-        /*$composer = $event->getComposer();
-        $io = $event->getIO();
-        $io->write('<info>Compiling containers list</info>');*/
-    }
+	}
+	
+	/**
+	 * Let's register the harmony dependencies update events.
+	 * 
+	 * @return array
+	 */
+	public static function getSubscribedEvents() {
+		return array(
+            ScriptEvents::POST_INSTALL_CMD => array(
+                array('postInstall', 0)
+            ),
+			ScriptEvents::POST_UPDATE_CMD => array(
+				array('postUpdate', 0)
+			),
+        );
+	}
+	
+
+	/**
+	 * Script callback; Acted on after install.
+	 */
+	public function postInstall(Event $event) {
+		self::processHarmonyDependencies($event, 'install');
+	}
+
+	/**
+	 * Script callback; Acted on after update.
+	 */
+	public function postUpdate(Event $event) {
+		self::processHarmonyDependencies($event, 'update');
+	}
+
+	/**
+	 * Script callback; Acted on after the autoloader is dumped.
+	 * 
+	 * @param Event $event
+	 * @param string $action update or install
+	 * @throws \Exception
+	 */
+	private static function processHarmonyDependencies(Event $event, $action) {
+		// Let's trigger EmbeddedComposer.
+		$composer = $event->getComposer();
+		$io = $event->getIO();
+		$io->write('');
+		$io->write('Updating Harmony dependencies');
+		$io->write('=============================');
+
+		$composerHarmonyFiles = [];
+
+		// Let's start by scanning all packages for a composer-harmony.json file.
+		//$localRepos = new CompositeRepository(array($composer->getRepositoryManager()->getLocalRepository()));
+		//$packagesList = $localRepos->getPackages();
+		$packagesList = $composer->getRepositoryManager()->getLocalRepository()
+				->getCanonicalPackages();
+		$packagesList[] = $composer->getPackage();
+
+		$globalHarmonyComposer = [];
+
+		foreach ($packagesList as $package) {
+			/* @var $package PackageInterface */
+			if ($package instanceof CompletePackage) {
+				if ($package instanceof RootPackage) {
+					$targetDir = "";
+				} else {
+					$targetDir = $package->getName() . "/";
+				}
+				if ($package->getTargetDir()) {
+					$targetDir .= $package->getTargetDir() . "/";
+				}
+
+				$composerFile = $targetDir . "composer-harmony.json";
+				if (file_exists($composerFile) && is_readable($composerFile)) {
+					$harmonyData = self::loadComposerHarmonyFile(
+							$composerFile, '../../../' . $targetDir);
+
+					$globalHarmonyComposer = array_merge_recursive(
+							$globalHarmonyComposer, $harmonyData);
+				}
+			}
+		}
+
+		$targetHarmonyFile = 'composer-harmony-dependencies.json';
+
+		if (file_exists($targetHarmonyFile) && !is_writable($targetHarmonyFile)) {
+			$io
+					->write(
+							"<error>Error, unable to write file '"
+									. $targetHarmonyFile
+									. "'. Please check file-permissions.</error>");
+			return;
+		}
+
+		if (!file_exists($targetHarmonyFile)
+				&& !is_writable(dirname($targetHarmonyFile))) {
+			$io
+					->write(
+							"<error>Error, unable to write a file in directory '"
+									. dirname($targetHarmonyFile)
+									. "'. Please check file-permissions.</error>");
+			return;
+		}
+
+		if ($globalHarmonyComposer) {
+			$result = file_put_contents($targetHarmonyFile,
+					json_encode($globalHarmonyComposer,
+							JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+			if ($result == false) {
+				throw new \Exception(
+						"An error occured while writing file '"
+								. $targetHarmonyFile . "'");
+			}
+			
+			// Run command
+			$oldCwd = getcwd();
+			chdir('vendor/mouf/mouf');
+			$commandLine = PHP_BINARY . " console.php composer:$action";
+			passthru($commandLine);
+			chdir($oldCwd);
+		} else {
+			$io->write("<info>No harmony dependencies to $action</info>");
+			if (file_exists($targetHarmonyFile)) {
+				$result = unlink($targetHarmonyFile);
+				if ($result == false) {
+					throw new \Exception(
+							"An error occured while deleting file '"
+							. $targetHarmonyFile . "'");
+				}
+			}
+			if (file_exists($targetHarmonyFile.".lock")) {
+				$result = unlink($targetHarmonyFile.".lock");
+				if ($result == false) {
+					throw new \Exception(
+							"An error occured while deleting file '"
+							. $targetHarmonyFile . ".lock'");
+				}
+			}
+		}
+
+	}
+
+	/**
+	 * Loads a harmony file, returns the array, with autoloads modified to fit the directory. 
+	 * 
+	 * @param string $composerHarmonyFile
+	 */
+	private static function loadComposerHarmonyFile($composerHarmonyFile,
+			$targetDir) {
+		$configJsonFile = new JsonFile($composerHarmonyFile);
+
+		try {
+			$configJsonFile->validateSchema(JsonFile::LAX_SCHEMA);
+			$localConfig = $configJsonFile->read();
+		} catch (ParsingException $e) {
+			throw new \Exception(
+					"Error while parsing file '" . $composerHarmonyFile . "'",
+					0, $e);
+		}
+
+		foreach (['autoload', 'autoload-dev'] as $autoloadType) {
+			foreach (['psr-4', 'psr-0', 'classmap', 'files'] as $mode) {
+				if (isset($localConfig[$autoloadType][$mode])) {
+					$localConfig[$autoloadType][$mode] = array_map(
+							function ($path) use ($targetDir) {
+								if (!is_array($path)) {
+									$path = [$path];
+								}
+								return array_map(
+										function ($pathItem) use ($targetDir) {
+											return $targetDir . $pathItem;
+										}, $path);
+							}, $localConfig[$autoloadType][$mode]);
+				}
+			}
+		}
+
+		return $localConfig;
+	}
+	
 }
