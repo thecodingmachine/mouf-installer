6d51a92e54c9b0d8207de15d72d0f2a818966ce1
David Négrier <d.negrier@thecodingmachine.com>
2018-08-02 11:59:06 +0200

    Adding vendor purge

---
+++ src/Mouf/Installer/MoufPlugin.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mouf\Installer;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\Factory;
+use Composer\IO\IOInterface;
+use Composer\Package\PackageInterface;
+use Composer\Plugin\Capability\CommandProvider;
+use Composer\Plugin\Capable;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\Event;
+use Composer\Script\ScriptEvents;
+use Symfony\Component\Filesystem\Filesystem;
+use TheCodingMachine\Discovery\Commands\CommandProvider as DiscoveryCommandProvider;
+use TheCodingMachine\Discovery\Commands\DumpCommand;
+
+class MoufPlugin implements PluginInterface, EventSubscriberInterface, Capable
+{
+    /**
+     * @var Composer
+     */
+    protected $composer;
+    protected $io;
+
+    /**
+     * Apply plugin modifications to Composer.
+     *
+     * @param Composer    $composer
+     * @param IOInterface $io
+     */
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        $this->composer = $composer;
+        $this->io = $io;
+        $moufFrameworkInstaller = new MoufFrameworkInstaller($io, $composer);
+        $composer->getInstallationManager()->addInstaller($moufFrameworkInstaller);
+        $moufLibraryInstaller = new MoufLibraryInstaller($io, $composer);
+        $composer->getInstallationManager()->addInstaller($moufLibraryInstaller);
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            ScriptEvents::PRE_AUTOLOAD_DUMP => 'beforeDumpAutoload',
+        ];
+    }
+
+    public function beforeDumpAutoload(Event $event)
+    {
+        // Plugin has been uninstalled
+        if (!file_exists(__FILE__)) {
+            return;
+        }
+
+        // Let's delete the vendor cache
+        $vendorCachePaths = array(__DIR__.'/../../../../../../mouf/no_commit/mouf_vendor_analysis.json'
+            , __DIR__.'/../../../../../../mouf/no_commit/vendor_analysis.json');
+
+        foreach ($vendorCachePaths as $vendorCachePath) {
+            if (\file_exists($vendorCachePath)) {
+                $result = \unlink($vendorCachePath);
+                if ($result === false) {
+                    throw new \Exception('Unable to delete cache file '.$vendorCachePath);
+                }
+            }
+        }
+    }
+}
